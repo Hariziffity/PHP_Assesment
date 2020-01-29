0a9c5d372a1d8f8881bf636d99275744d9bed9da
Hari Vijay Sudharsun <hari.vijay@ziffity.com>
2020-01-29 16:27:30 +0530

    Pagination, Search all improvemnts

--- products.php
+++ products.php
@@ -1,23 +1,5 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <script>
-            function onLinkClick(x) {
-                // var xhttp = new XMLHttpRequest();
-                // xhttp.onreadystatechange = function() {
-                //     if(this.readyState == 4 && this.status == 200) {
-                //         console.log('done');
-                //     }
-                // };
-
-                // xhttp.open("GET", "products.php?page=" + x, true);
-                // xhttp.send();
-
-                document.getElementById(x).className = 'active';
-                // event.preventDefault();
-            }
-        </script>
-    </head>
     <body>
     <?php
         include 'header.php';
@@ -27,7 +9,13 @@
     <?php
         $products_file = 'products.json';
         $products_data = file_get_contents($products_file);
-        $products = json_decode($products_data);     
+        $products = json_decode($products_data, true);     
+    ?>
+
+    <?php
+        function getPages($items, $limit){
+            return ceil($items / $limit);
+        }
     ?>
 
     <div class="pagination">
@@ -37,14 +25,19 @@
             
                 $page = !isset($_GET['page']) ? 1 : $_GET['page'];
 
-                $limit = 5;
-                $total_items = count($products);
-                $total_pages = ceil($total_items / $limit);
-
-                for($x = 1; $x <= $total_pages ; $x++) {
-                    echo "<a id=$x href='products.php?search=&categories=all&show=show_specific&page=$x' onclick='onLinkClick($x)'>$x</a>";
+                if ($_GET['show'] === 'show_specific') {
+                    $total_pages = 1;
+                } else if ($_GET['show'] === 'show_all') {
+                    $limit = 5;
+                    $total_items = count($products);
+                    $total_pages = ceil($total_items / $limit);
                 }
 
+                for($x = 1; $x <= $total_pages ; $x++) {
+                    $activeClass = ($x == $page) ? 'active' : '';
+                    $showValue = $_GET['show'];
+                    echo "<a id=$x href='products.php?search=&categories=all&show=$showValue&page=$x' class=$activeClass>$x</a>";
+                }
 
             ?>
         </ul>
@@ -67,35 +60,48 @@
                 </th>
             </tr>
 
-            
-
             <?php
                 if($_GET['show'] === 'show_specific') {
 
                     $page = !isset($_GET['page']) ? 1 : $_GET['page'];
 
+                    $category = !isset($_GET['categories']) ? 'all' : $_GET['categories'];
+
+                    $specific_products = array_filter($products, function($var) use ($category) {
+                        if ($category === 'all'){
+                            return $var;
+                        }
+                        return ($var['category'] == $category);
+                    });
+
                     $limit = 5;
                     $offset = ($page - 1) * $limit;
-                    $total_items = count($products);
+                    $total_items = count($specific_products);
                     $total_pages = ceil($total_items / $limit);
-                    $specific_products = array_splice($products, $offset, $limit);
+                    $final_specific_products = array_splice($specific_products, $offset, $limit);
 
-                    foreach ($specific_products as $product) : 
+                    foreach ($final_specific_products as $product) : 
                         echo "<tr>";
-                        echo "<td>" . $product->name . "</td>";
-                        echo "<td>" . $product->skuid . "</td>";
-                        echo "<td>" . $product->price . "</td>";
+                        echo "<td>" . $product['name'] . "</td>";
+                        echo "<td>" . $product['skuid'] . "</td>";
+                        echo "<td>" . $product['price'] . "</td>";
                         echo "<td><input name=number type=number value=0 min=0 max= 1000></td>";
                         echo "</tr>";
                     endforeach;
 
                 } else if ($_GET['show'] === 'show_all') {
 
-                    foreach ($products as $product) : 
+                    $limit = 5;
+                    $offset = ($page - 1) * $limit;
+                    $total_items = count($products);
+                    $total_pages = ceil($total_items / $limit);
+                    $final_specific_products = array_splice($products, $offset, $limit);
+
+                    foreach ($final_specific_products as $product) : 
                         echo "<tr>";
-                        echo "<td>" . $product->name . "</td>";
-                        echo "<td>" . $product->skuid . "</td>";
-                        echo "<td>" . $product->price . "</td>";
+                        echo "<td>" . $product['name'] . "</td>";
+                        echo "<td>" . $product['skuid'] . "</td>";
+                        echo "<td>" . $product['price'] . "</td>";
                         echo "<td><input name=number type=number value=0 min=0 max= 1000></td>";
                         echo "</tr>";
                     endforeach;
